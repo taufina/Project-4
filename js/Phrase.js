abf34420977a75b67a8eb1319f76bd6a6cbe92a3
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-07-10 09:15:53 -0500

    Project set up

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+      this.phrase = phrase.toLowerCase();
+    }
+
+    // to add letter placeholders to the display when the game starts.  
+    //each letter represented by an empty box.  one li element for each letter.
+    addPhraseToDisplay(){};
+
+    // check to see if the letter selected by the player matches a letter in the phrase.
+    checkLetter(){};
+
+    //reveal the letters on the board that matches the player's selection. 
+    showMatchedLetter(){};
+  }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+};
+
+const game = new Game();
+const randomPhrase = game.getRandomPhrase();
+//const phrase = new Phrase(randomPhrase.phrase);
+randomPhrase.addPhraseToDisplay;
+
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+
+
+// Create a new instance of the Game class and add event listeners for the start button and onscreen keyboard buttons:
+// Add a click event listener to the "Start Game" button which creates a new Game object and starts the game by calling 
+// the startGame() method.
+// Add click event listeners to each of the onscreen keyboard buttons, so that clicking a button calls the 
+// handleInteraction() method on the Game object. Event delegation can also be used in order to avoid having to add an 
+// event listener to each individual keyboard button. Clicking the space between and around the onscreen keyboard buttons
+// should not result in the handleInteraction() method being called.
+
